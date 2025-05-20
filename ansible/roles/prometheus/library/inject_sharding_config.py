951a642ed8315992cadee9aa5caa0de52acd9be2
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2025-05-20 12:48:37 +0700

    sharding for json file_sd

--- ansible/roles/prometheus/library/inject_sharding_config.py
+++ ansible/roles/prometheus/library/inject_sharding_config.py
@@ -1,31 +1,41 @@
 from ansible.module_utils.basic import AnsibleModule
 import yaml
+import json
 import os
 
 def load_config_file(path):
-    with open(path, 'r') as f:
-        return yaml.safe_load(f)
+    _, ext = os.path.splitext(path)
+    if ext in ['.yaml', '.yml']:
+        with open(path, 'r') as f:
+            return yaml.safe_load(f)
+    elif ext == '.json':
+        with open(path, 'r') as f:
+            return json.load(f)
 
 def save_config_file(data, path):
-    with open(path, 'w') as f:
-        yaml.dump(data, f, default_flow_style=False)
+    _, ext = os.path.splitext(path)
+    if ext in ['.yaml', '.yml']:
+        with open(path, 'w') as f:
+            yaml.dump(data, f, default_flow_style=False)
+    elif ext == '.json':
+        with open(path, 'w') as f:
+            json.dump(data, f, indent=2, ensure_ascii=False)
 
 def add_sharding(config, modulus, hash_value):
-    if 'scrape_configs' in config.keys():
-        for job in config['scrape_configs']:
-            job['relabel_configs'] = [
-                {
-                    'source_labels': ['__address__'],
-                    'modulus': modulus,
-                    'target_label': '__tmp_hash__',
-                    'action': 'hashmod'
-                },
-                {
-                    'source_labels': ['__tmp_hash__'],
-                    'regex': hash_value,
-                    'action': 'keep'
-                }
-            ]
+    for job in config.get('scrape_configs', []):
+        job['relabel_configs'] = [
+            {
+                'source_labels': ['__address__'],
+                'modulus': modulus,
+                'target_label': '__tmp_hash__',
+                'action': 'hashmod'
+            },
+            {
+                'source_labels': ['__tmp_hash__'],
+                'regex': hash_value,
+                'action': 'keep'
+            }
+        ]
     return config
 
 def main():
@@ -42,10 +52,13 @@ def main():
     hash_value = module.params['hash_value']
 
     config = load_config_file(source)
+    if not isinstance(config, dict):
+        module.exit_json(changed=False)
+        return
     sharded_config = add_sharding(config, modulus, hash_value)
-
     save_config_file(sharded_config, source)
-    module.exit_json(changed=True, msg='Sharding configurations added')
+
+    module.exit_json(changed=True)
 
 if __name__ == '__main__':
     main()
